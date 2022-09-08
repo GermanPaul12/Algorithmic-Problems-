fec78e84aa0d75fe2f4897dff37878ffe276ceab
German Paul <99292019+GermanPaul12@users.noreply.github.com>
2022-09-08 17:52:15 +0200

    Add files via upload

---
+++ binary-search.py
@@ -0,0 +1,11 @@
+def binary_search(lo, hi, condition):
+    while lo <= hi:
+        mid = (lo + hi) // 2
+        result = condition(mid)
+        if result == 'found':
+            return mid
+        elif result == 'left':
+            hi = mid - 1
+        else:
+            lo = mid + 1
+    return -1
